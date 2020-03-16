223a1fcd87aeb881bf16e5e82f9842fed013ceb7
shobrook <shobrookj@gmail.com>
2020-03-16 18:37:03 -0500

    Revamped cryptform view

--- quicklock/src/components/FileHeader.js
+++ quicklock/src/components/FileHeader.js
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 import "./FileHeader.css";
 
 export default class FileHeader extends Component {
-  render() {
-    const { fileName } = this.props;
+	render() {
+		const { fileName } = this.props;
 
-    return (
-      <div className="fileHeader">
-        <div className="fileName">
-          <img src="./sampleFileIcon.png" />
-          <span>{fileName}</span>
-        </div>
-        <div className="border" />
-      </div>
-    );
-  }
+		return (
+			<div className="fileHeader">
+				<div className="fileName">
+					<img src="./sampleFileIcon.png" />
+					<span>{fileName}</span>
+				</div>
+			</div>
+		);
+	}
 }
