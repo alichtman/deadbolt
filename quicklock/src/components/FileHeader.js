25d8edee647725d4309d7776aa10dcccd6827bc6
shobrook <shobrookj@gmail.com>
2020-03-17 00:39:45 -0500

    Added automatic file icon rendering

--- quicklock/src/components/FileHeader.js
+++ quicklock/src/components/FileHeader.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import "./FileHeader.css";
 
+import "file-icons-js/css/style.css";
+import icons from "file-icons-js";
+
 export default class FileHeader extends Component {
 	render() {
 		const { fileName } = this.props;
+		const iconClassName = icons.getClass(fileName);
 
 		return (
 			<div className="fileHeader">
 				<div className="fileName">
-					<img src="./sampleFileIcon.png" />
-					<span>{fileName}</span>
+					<span className="filePathWrapper">
+						<span className={iconClassName} id="fileIcon" />
+						<span className="filePath">{fileName}</span>
+					</span>
 				</div>
 			</div>
 		);
