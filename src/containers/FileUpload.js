ffd8b966a27671ea48dccebb38aeb9365b367710
shobrook <shobrookj@gmail.com>
2020-03-19 19:21:25 -0500

    Added version

--- src/containers/FileUpload.js
+++ src/containers/FileUpload.js
@@ -1,5 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./FileUpload.css";
+
+const { version } = require("../../package.json");
+// const path = require("path");
+// const packageJSON = path.dirname(require.resolve("deadbolt")) + "/package.json";
+// const { version } = require(packageJSON);
 
 export default class FileUpload extends Component {
 	constructor(props) {
@@ -20,36 +25,39 @@ export default class FileUpload extends Component {
 		const { setFilePath } = this.props;
 
 		return (
-			<div
-				className="fileUpload"
-				onDragOver={this.onDragOver}
-				onDragLeave={() => false}
-				onDragEnd={() => false}
-				onDrop={event => {
-					event.preventDefault();
-					let file = event.dataTransfer.files[0];
-
-					return setFilePath(file);
-				}}
-				onClick={this.onClick}
-			>
-				<input
-					type="file"
-					ref="fileUploader"
-					style={{ display: "none" }}
-					onChange={event => {
-						event.stopPropagation();
+			<Fragment>
+				<div
+					className="fileUpload"
+					onDragOver={this.onDragOver}
+					onDragLeave={() => false}
+					onDragEnd={() => false}
+					onDrop={event => {
 						event.preventDefault();
-						let file = event.target.files[0];
+						let file = event.dataTransfer.files[0];
 
 						return setFilePath(file);
 					}}
-				/>
-				<div className="fileUploadIcon">
-					<img src="./dropFileIcon.svg" />
+					onClick={this.onClick}
+				>
+					<input
+						type="file"
+						ref="fileUploader"
+						style={{ display: "none" }}
+						onChange={event => {
+							event.stopPropagation();
+							event.preventDefault();
+							let file = event.target.files[0];
+
+							return setFilePath(file);
+						}}
+					/>
+					<div className="fileUploadIcon">
+						<img src="./dropFileIcon.svg" />
+					</div>
+					<span className="fileUploadText">Select or Drop</span>
 				</div>
-				<span className="fileUploadText">Select or Drop</span>
-			</div>
+				<span className="versionTag">{`v${version}`}</span>
+			</Fragment>
 		);
 	}
 }
