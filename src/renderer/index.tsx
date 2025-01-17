37bb02cc5c754243126ddef493e758cc647a31cd
Aaron Lichtman <aaronlichtman@gmail.com>
2025-01-17 03:44:21 -0800

    Add styling for password inputs

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,33 @@
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1d7be0',
+      contrastText: '#FFF',
+      dark: '#aa3517',
+    },
+  },
+
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiInputBase-input::placeholder': {
+            color: '#FFFFFF',
+            opacity: 0.65,
+          },
+        },
+      },
+    },
+  },
+});
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+);
