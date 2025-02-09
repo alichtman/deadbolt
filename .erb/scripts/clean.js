b0a4afa72f5ee31669c766324fbad30da490e501
Aaron Lichtman <aaronlichtman@gmail.com>
2025-02-08 17:52:01 -0800

    build: maybe fix missing macOS artifact bug?
    In https://github.com/alichtman/deadbolt/actions/runs/13221108680/job/36906249381,
    the macOS build LOOKS successful, but no build artifacts were ever actually uploaded:
    
    ```
    Run actions/upload-artifact@v4
    Warning: No files were found with the provided path: release/build/*.dmg. No artifacts will be uploaded.
    ```
    
    It looks like the windows build started after, so my guess is that
    they're running in the same filesystem, and the "clean on build start"
    code is messing things up.
    
    This is only really needed for development, so we'll just skip it in CI.

--- .erb/scripts/clean.js
+++ .erb/scripts/clean.js
@@ -2,6 +2,12 @@ import { rimrafSync } from 'rimraf';
 import fs from 'fs';
 import webpackPaths from '../configs/webpack.paths';
 
+// Don't clean anything in CI. We guarantee that the dist/ directory is clean bc we just cloned the repo.
+if (process.env.CI) {
+  console.log('Skipping clean in CI environment');
+  process.exit(0);
+}
+
 const foldersToRemove = [
   webpackPaths.distPath,
   webpackPaths.buildPath,
